22ee54750188420d42452e478f6ead1861c5b10f
Aandi1989 <navaliny1989@gmail.com>
2023-07-13 10:46:15 +0300

    API delete request

---
+++ app/api/posts/[id]/route.ts
@@ -0,0 +1,8 @@
+import { NextResponse } from "next/server"
+
+export async function DELETE(req: Request, {params}:{params: {id:string} } ){
+    const id = params.id
+
+    // logic delete post
+    return NextResponse.json({id});
+}
